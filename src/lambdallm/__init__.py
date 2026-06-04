1b2804f4ea0421c5d4074bb974fc915013f36040
Gaurav Kumar Sinha <gaurav@substrai.dev>
2026-06-04 08:23:59 -0500

    chore: bump version to 1.8.0 for PyPI release
    Signed-off-by: Gaurav Kumar Sinha <gaurav@substrai.dev>

--- src/lambdallm/__init__.py
+++ src/lambdallm/__init__.py
@@ -8,7 +8,7 @@ Docs: https://docs.substrai.dev/lambdallm
 Author: Gaurav Kumar Sinha <gaurav@substrai.dev>
 """
 
-__version__ = "1.7.0"
+__version__ = "1.8.0"
 __author__ = "Gaurav Kumar Sinha"
 __email__ = "gaurav@substrai.dev"
 
